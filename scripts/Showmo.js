12971843dd11e02fe58a62f4a93605333324a481
IevhenBuival <kdotbull@gmail.com>
2023-03-05 16:02:26 +0200

    format files

---
+++ scripts/Showmo.js
@@ -0,0 +1,19 @@
+
+let myAbout = document.getElementById('aboutwithShowMoAll');
+let StartState = myAbout.classList.length;
+let myAboutLink = document.getElementById('aboutwithShowMo');
+if (myAbout.classList.length === 0) {
+    myAbout.classList.add('invisible');
+};
+let myAboutLinkHref = document.getElementById('aboutwithShowMoHref');
+
+myAboutLink.addEventListener('click', function (e) {
+    if (myAbout.classList.length === 0) {
+        myAbout.classList.add('invisible');
+        myAboutLinkHref.textContent = "Show All";
+    } else {
+        myAbout.classList.remove('invisible');
+        myAboutLinkHref.textContent = "Show les";
+    }
+}, false);
+
