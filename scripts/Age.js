2b82b98f040d2f3ee361440f830c04fecd1aea28
IevhenBuival <kdotbull@gmail.com>
2023-06-27 17:17:52 +0300

    add AWS project

--- scripts/Age.js
+++ scripts/Age.js
@@ -1,16 +1,19 @@
 let myActualDate = new Date();
 const myBirthdayv = new Date("1982-04-07");
-let myAge = document.getElementById('myAge');
+let myAge = document.getElementById("myAge");
 
 let diff = (myActualDate - myBirthdayv) / (60 * 60 * 24 * 365 * 1000);
 let years = Math.floor(diff);
-myAge.textContent = 'Age ' + years;
-let myh2_AGE = document.getElementById('h2_AGE');
-myh2_AGE.addEventListener('click', function (e) {
-  if (myAge.textContent === 'Age ' + years) {
-    myAge.textContent = 'Birthday: 1982-04-07';
-  } else {
-    myAge.textContent = 'Age ' + years;
-  }
-}, false);
-
+myAge.textContent = "Birthday: 1982-04-07";
+let myh2_AGE = document.getElementById("h2_AGE");
+myh2_AGE.addEventListener(
+  "click",
+  function (e) {
+    if (myAge.textContent === "Age " + years) {
+      myAge.textContent = "Birthday: 1982-04-07";
+    } else {
+      myAge.textContent = "Age " + years;
+    }
+  },
+  false
+);
